6e1b6e25871a3784231242c2739364a96172ecce
kahverengi <ahmet@duruer.dev>
2024-04-02 23:36:26 +0300

    chore: remove useless instance code

--- src/main/kotlin/com/panomc/plugins/announcement/AnnouncementPlugin.kt
+++ src/main/kotlin/com/panomc/plugins/announcement/AnnouncementPlugin.kt
@@ -3,14 +3,6 @@ package com.panomc.plugins.announcement
 import com.panomc.platform.api.PanoPlugin
 
 class AnnouncementPlugin : PanoPlugin() {
-    companion object {
-        internal lateinit var INSTANCE: AnnouncementPlugin
-    }
-
-    override suspend fun onLoad() {
-        INSTANCE = this
-    }
-
     override suspend fun onEnable() {
         logger.info("Started!")
     }
