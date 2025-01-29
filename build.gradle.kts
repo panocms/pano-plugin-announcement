8df32f2f25f5b6214d4d553898a4ed2e2878dae0
kahverengi <ahmet@duruer.dev>
2025-01-29 21:01:42 +0300

    fix: try to fix not building due to not correct pano repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     if (bootstrap) {
         compileOnly(project(mapOf("path" to ":Pano")))
     } else {
-        compileOnly("com.github.panocms:pano:alpha-SNAPSHOT")
+        compileOnly("com.github.panomc:pano:alpha-SNAPSHOT")
     }
 
     compileOnly(kotlin("stdlib-jdk8"))
