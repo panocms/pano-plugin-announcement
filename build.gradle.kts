d26b514cb62865613ebf7893a8e5c3bfd6234dee
kahverengi <ahmet@duruer.dev>
2025-01-30 11:15:25 +0300

    fix: fix not deploying, use Pano v1.0.0-alpha.69

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     if (bootstrap) {
         compileOnly(project(mapOf("path" to ":Pano")))
     } else {
-        compileOnly("com.github.panomc:pano:v1.0.0-alpha.68")
+        compileOnly("com.github.panomc:pano:v1.0.0-alpha.69")
     }
 
     compileOnly(kotlin("stdlib-jdk8"))
